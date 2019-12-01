fee9c1951be23dbb0115edd1d805bad1804aae87
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2019-12-01 18:48:37 +0700

    Initial commit

---
+++ lib/App/TouchpadUtils.pm
@@ -0,0 +1,15 @@
+package App::TouchpadUtils;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: CLI utilities related to touchpad
+
+=head1 DESCRIPTION
+
+This distribution contains the following CLI utilities related to touchpad:
+
+# INSERT_EXECS_LIST
